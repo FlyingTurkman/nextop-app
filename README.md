e11d6868d0e182db54b9cd8bc61e530806e0d594
Sharkman <dytdenizarli@hotmail.com>
2026-01-05 21:33:05 +0300

    update: create-nextop-app template main page updated

--- README.md
+++ README.md
@@ -2,10 +2,15 @@
 
 **NextOP** is the ultimate bridge between **Next.js** and **Electron**. It combines the rapid development experience of Next.js with the powerful native capabilities of Electron to help you build high-performance desktop applications with ease.
 
-## 🛠️ Quick Start
+## Quick Start
 
 To create your first NextOP project, simply run the following command in your terminal:
 
 ```bash
 npx create-nextop-app app-name
 ```
+
+
+## nextop-app hooks
+
+
--- test/app/page.tsx
+++ test/app/page.tsx
@@ -1,6 +1,8 @@
+'use client'
+
 import Image from 'next/image'
 import Link from 'next/link'
-
+import { useFs, useWindow } from 'nextop-app'
 
 
 
@@ -10,6 +12,9 @@ import Link from 'next/link'
 
 
 export default function Page() {
+
+    const { writeFile } = useFs()
+    const { close, maximize, minimize } = useWindow()
 
     return (
         <main
@@ -29,6 +34,43 @@ export default function Page() {
             <label>
                 For contrubition <Link href={'https://github.com/FlyingTurkman/nextop-app'} className='underline text-lg font-semibold'>NextOP</Link>
             </label>
+            <button
+            className='p-2 bg-[#1a1a1a] text-white rounded-md cursor-pointer'
+            onClick={() => {
+                writeFile('nextop.txt', 'This is a text file created by nextop')
+            }}
+            >
+                Create a text file
+            </button>
+            <div
+            className='flex flex-row items-center gap-4'
+            >
+                <button
+                className='p-2 bg-[#1a1a1a] text-white rounded-md cursor-pointer'
+                onClick={() => {
+                    minimize()
+                }}
+                >
+                    Minimize
+                </button>
+                <button
+                className='p-2 bg-[#1a1a1a] text-white rounded-md cursor-pointer'
+                onClick={() => {
+                    maximize()
+                }}
+                >
+                    Maximize
+                </button>
+                <button
+                className='p-2 bg-[#1a1a1a] text-white rounded-md cursor-pointer'
+                onClick={() => {
+                    close()
+                }}
+                >
+                    Close
+                </button>
+
+            </div>
         </main>
     )
 }
